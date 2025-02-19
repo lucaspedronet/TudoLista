02a6a45cc8acf9749f15bb5ef35e7b9f06b3f60c
Lucas Pedro <lucaspedrolbg@gmail.com>
2025-02-19 16:22:29 -0300

    feat: criando nova func handleAddNewTask

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,44 @@ import { PlusCircle } from '@phosphor-icons/react'
 import styles from './App.module.css'
 
 import { Button, Input, Empty, Item, Header } from './components'
-import { useState } from 'react'
-
 export interface ITask {
   id: number
   text: string
   isChecked: boolean
 }
 
+const listaDeTarefas: ITask[] = [
+  {
+    id: 1,
+    text: 'Estudar React',
+    isChecked: false
+  },
+  {
+    id: 2,
+    text: 'Enviar e-mail para o cliente',
+    isChecked: false
+  },
+  {
+    id: 3,
+    text: 'Levar o cachorro para passear',
+    isChecked: false
+  },
+];
+
 export function App() {
-  const [tasks, setTasks] = useState<ITask[]>([]);
-  const [inputValue, setInputValue] = useState('');
+
+  function handleAddNewTask() {
+
+    const newTask: ITask = {
+      id: Math.random(),
+      text: 'Nova tarefa',
+      isChecked: false
+    };
+
+    listaDeTarefas.push(newTask);
+    console.log("Lita de Tarefas: ", listaDeTarefas);
+    
+  }
 
   return (
     <main>
@@ -23,21 +50,21 @@ export function App() {
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
           <Input
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
+            onChange={(e) => {}}
+            value={''}
           />
-          <Button onClick={() => {}}>
+          <Button onClick={handleAddNewTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </Button>
         </div>
 
         <div className={styles.tasksList}>
-          {[].length > 0 ? (
+          {listaDeTarefas.length > 0 ? (
             <div>
-              {[].map((task) => (
+              {listaDeTarefas.map((task) => (
                 <Item
-                  key={task}
+                  key={task.id}
                   data={task}
                   removeTask={() => {}}
                   toggleTaskStatus={() => {}}
