6c14169578ddd244860a2975d329fefec4aa6afc
Lucas Pedro <lucaspedrolbg@gmail.com>
2025-02-21 16:15:47 -0300

    fix: preparando aula sobre estados no reactjs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { PlusCircle } from '@phosphor-icons/react'
 import styles from './App.module.css'
 
 import { Button, Input, Empty, Item, Header } from './components'
-import { useState } from 'react'
 export interface ITask {
   id: number
   text: string
@@ -34,26 +33,6 @@ const listaDeTarefas: ITask[] = [
 ];
 
 export function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Estudar React',
-      isChecked: false
-    }
-  ]);
-
-  function handleAddNewTask() {
-
-    const newTask: ITask = {
-      id: Math.random(),
-      text: 'Nova tarefa',
-      isChecked: false
-    };
-
-    setTasks(nexState => [...nexState, newTask]);
-    
-    console.log("tasks: ", tasks);
-  }
 
   return (
     <main>
@@ -63,21 +42,21 @@ export function App() {
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
           <Input
-            onChange={(e) => {}}
+            onChange={() => {}}
             value={''}
           />
-          <Button onClick={handleAddNewTask}>
+          <Button onClick={() => {}}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </Button>
         </div>
 
         <div className={styles.tasksList}>
-          {tasks.length > 0 ? (
+          {[].length > 0 ? (
             <div>
-              {tasks.map((task) => (
+              {[].map((task) => (
                 <Item
-                  key={task.id}
+                  key={task}
                   data={task}
                   removeTask={() => {}}
                   toggleTaskStatus={() => {}}
