598baaa4b372f6118d6c843f601be425a58bf3d3
Lucas Pedro <lucaspedrolbg@gmail.com>
2025-02-26 15:01:44 -0300

    fix: exemplo de state

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { PlusCircle } from '@phosphor-icons/react'
 import styles from './App.module.css'
 
 import { Button, Input, Empty, Item, Header } from './components'
+import { useState } from 'react';
 export interface ITask {
   id: number
   text: string
@@ -11,9 +12,9 @@ export interface ITask {
 
 const listaDeTarefas: ITask[] = [
   {
-    id: 1,
+    id: 1878,
     text: 'Estudar React',
-    isChecked: false
+    isChecked: false,
   },
   {
     id: 2,
@@ -27,7 +28,27 @@ const listaDeTarefas: ITask[] = [
   },
 ];
 
+
 export function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function handleNewAddTask() {
+    const newTask: ITask = {
+      id: Math.random(), // número aleatório
+      text: 'Task nova pessoal',
+      isChecked: false,
+    };
+
+    // nextState => [...nextState, newTask]
+    setTasks((nextState) => {
+      const newState = [...nextState, newTask];
+      return newState;
+    });
+
+
+  
+    console.log("Tasks: ", tasks);
+  }
 
   return (
     <main>
@@ -40,23 +61,25 @@ export function App() {
             onChange={() => {}}
             value={''}
           />
-          <Button onClick={() => {}}>
+          <Button onClick={handleNewAddTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </Button>
         </div>
 
         <div className={styles.tasksList}>
-          {listaDeTarefas.length > 0 ? (
+          {tasks.length > 0 ? (
             <div>
-              {listaDeTarefas.map((task) => (
-                <Item
-                  key={task.id}
-                  data={task}
-                  removeTask={() => {}}
-                  toggleTaskStatus={() => {}}
-                />
-              ))}
+              {tasks.map(function nomeDaFuncao(task) {
+                return (
+                  <Item
+                    key={task.id}
+                    data={task}
+                    removeTask={() => {}}
+                    toggleTaskStatus={() => {}}
+                  />
+                )
+              })}
             </div>
           ) : (
             <Empty />
