63a153c26d6c5afd04c798451b8146477411f1d6
Lucas Pedro <lucaspedrolbg@gmail.com>
2025-02-28 14:54:49 -0300

    fix: ajustes para aula 04

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,33 @@ import { PlusCircle } from '@phosphor-icons/react'
 import styles from './App.module.css'
 
 import { Button, Input, Empty, Item, Header } from './components'
-import { useState } from 'react';
 export interface ITask {
   id: number
   text: string
   isChecked: boolean
 }
 
+const listaDeTarefas: ITask[] = [
+  {
+    id: 1,
+    text: 'Estudar React',
+    isChecked: false,
+  },
+  {
+    id: 2,
+    text: 'Estudar TypeScript',
+    isChecked: false,
+  },
+  {
+    id: 3,
+    text: 'Estudar CSS',
+    isChecked: false,
+  },
+];
+
 export function App() {
-  const [tasks, setTasks] = useState<ITask[]>([
-    {
-      id: 1,
-      text: 'Demo',
-      isChecked: false,
-    }
-  ]);
-  const [inputName, setInputName] = useState('');
 
   function handleNewAddTask() {
-    if (inputName.trim().length === 0) {
-      return;
-    }
-
-    const newTask: ITask = {
-      id: Math.random(),
-      text: inputName,
-      isChecked: false,
-    };
-
-    setTasks((test) => [...test, newTask]);
-
-    setInputName('')
   }
 
   return (
@@ -44,8 +40,8 @@ export function App() {
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
           <Input
-            onChange={(e) => setInputName(e.target.value)}
-            value={inputName}
+            onChange={() => {}}
+            value={''}
           />
           <Button onClick={handleNewAddTask}>
             Criar
@@ -54,9 +50,9 @@ export function App() {
         </div>
 
         <div className={styles.tasksList}>
-          {tasks.length > 0 ? (
+          {listaDeTarefas.length > 0 ? (
             <div>
-              {tasks.map(function nomeDaFuncao(task) {
+              {listaDeTarefas.map((task) => {
                 return (
                   <Item
                     key={task.id}
