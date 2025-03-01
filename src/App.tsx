8f1c93aa3a1b74e9c73cb938475b26b2de68dfe1
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2025-02-28 21:26:43 -0300

    alteracao

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ import { Button } from './components/Button'
 import { Header, Header2 } from './components/Header'
 import { Input } from './components/Input'
 import { Empty } from './components/List/Empty'
-import { Header as ListHeader } from './components/List/Header'
+//import { Header as ListHeader } from './components/List/Header'
 import { Item } from './components/List/Item'
+
+
 
 export interface ITask {
   id: number
@@ -16,42 +18,63 @@ export interface ITask {
   isChecked: boolean
 }
 
+const initialState: ITask[] = [];
+
 export function App() {
-  const [tasks, setTasks] = useState<ITask[]>([])
-  const [inputValue, setInputValue] = useState('');
+
+  const [tasks, setTasks] = useState(initialState);
+  const [inputName, setInputName] = useState("");
+
+  function handleNewAddTask() {
+    if(inputName.trim().length <= 0 ){
+      return;
+    }
+
+    const existTask = tasks.find(t => t.text === inputName);
+
+    if (existTask){
+      return;
+    }
+
+    const newTask: ITask = {
+      id: Math.random(),
+      text: inputName,
+      isChecked: false,
+    };
+    setTasks((prevState) => [...prevState, newTask]);
+  }
 
   return (
     <main>
+
       <Header />
 
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
           <Input
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
+            onChange={(e) => setInputName(e.target.value)}
+            value={inputName}
           />
-          <Button onClick={() => {}}>
+          <Button onClick={handleNewAddTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </Button>
         </div>
 
         <div className={styles.tasksList}>
-          <ListHeader
-            tasksCounter={tasks.length}
-            checkedTasksCounter={0}
-          />
-
-          {tasks.length > 0 ? (
+           {
+          tasks.length > 0 ? (
             <div>
-              {tasks.map((task) => (
-                <Item
-                  key={task.id}
-                  data={task}
-                  removeTask={() => {}}
-                  toggleTaskStatus={() => {}}
-                />
-              ))}
+              {tasks.map((task) => {
+                return (
+                  <Item
+                    key={task.id}
+                    data={task}
+                    removeTask={() => {}}
+                    toggleTaskStatus={() => {}}
+                  />
+                )
+              })}
             </div>
           ) : (
             <Empty />
