8e251866983cdc2d99fa1652d20968123b2945f3
Laboratorios de Informatica CEULP <labins.pmw@ensino.ulbra.net>
2025-02-28 21:52:42 -0300

    Gerencidador de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -3,57 +3,63 @@ import { PlusCircle } from '@phosphor-icons/react'
 import styles from './App.module.css'
 
 import { Button, Input, Empty, Item, Header } from './components'
+import { useState } from 'react'
+
 export interface ITask {
   id: number
   text: string
   isChecked: boolean
 }
 
-const listaDeTarefas: ITask[] = [
-  {
-    id: 1,
-    text: 'Estudar React',
-    isChecked: false
-  },
-  {
-    id: 2,
-    text: 'Enviar e-mail para o cliente',
-    isChecked: false
-  },
-  {
-    id: 3,
-    text: 'Levar o cachorro para passear',
-    isChecked: false
-  },
-];
-
 export function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [inputName, setInputName] = useState('')
+
+  function addTask() {
+    if (inputName.trim().length <= 0) {
+      return;
+    }
+    const existTask = tasks.find(t => t.text === inputName);
+    if (existTask) {
+      return;
+    }
+    const newTask: ITask = {
+      id: Math.random(),
+      text: inputName,
+      isChecked: false,
+    }
+    setTasks((prevState) => [...prevState, newTask]);
+    setInputName('');
+  }
+
+  function removeTask(id: number) {
+    setTasks((prevTasks) => prevTasks.filter(task => task.id !== id));
+  }
 
   return (
     <main>
-
       <Header />
 
       <section className={styles.content}>
         <div className={styles.taskInfoContainer}>
           <Input
-            onChange={() => {}}
-            value={''}
+            onChange={(e) => setInputName(e.target.value)}
+            value={inputName}
           />
-          <Button onClick={() => {}}>
+          <Button onClick={addTask}>
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </Button>
         </div>
 
         <div className={styles.tasksList}>
-          {listaDeTarefas.length > 0 ? (
+          {tasks.length > 0 ? (
             <div>
-              {listaDeTarefas.map((task) => (
+              {tasks.map((task) => (
                 <Item
                   key={task.id}
                   data={task}
-                  removeTask={() => {}}
+                  removeTask={() => removeTask(task.id)}
                   toggleTaskStatus={() => {}}
                 />
               ))}
