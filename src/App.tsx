7c7b60366516c6885f196b40ac50634a1e88ce2c
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2025-03-12 19:46:45 -0300

    UP

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { PlusCircle } from '@phosphor-icons/react'
+<<<<<<< Updated upstream
 import { useActionState, useEffect, useState } from 'react'
 import PropTypes from 'prop-types';
 import styles from './App.module.css'
@@ -10,14 +11,24 @@ import { Empty } from './components/Empty/Empty'
 import { Header as ListHeader } from './components/Header/Header'
 import { Item } from './components/Item/Item'
 
+=======
+
+import styles from './App.module.css'
+
+import { Button, Input, Empty, Item, Header } from './components'
+import { useState } from 'react';
+>>>>>>> Stashed changes
 export interface ITask {
   id: number;
   text: string;
   isChecked: boolean;
 }
 
+
+
 export function App() {
   const [tasks, setTasks] = useState<ITask[]>([]);
+<<<<<<< Updated upstream
   const [inputName, setInputName] = useState("");
   const [TasksCount, setTasksCount] = useState(0);
   function addTask() {
@@ -31,12 +42,20 @@ export function App() {
     const newTask: ITask = {
       id: Math.random(),
       text: inputName,
+=======
+
+  function handleNewAddTask() {
+    const newTask: ITask = {
+      id: Math.random(), // número aleatório
+      text: 'Task nova pessoal',
+>>>>>>> Stashed changes
       isChecked: false,
     };
     setTasks((prevState) => [...prevState, newTask]);
     setInputName("  ");
   }
 
+<<<<<<< Updated upstream
   function removeTask(id: number) {
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   }
@@ -87,3 +106,53 @@ export function App() {
     );
   }
 
+=======
+    // nextState => [...nextState, newTask]
+    setTasks((nextState) => {
+      const newState = [...nextState, newTask];
+      return newState;
+    });
+    
+    console.log("Tasks: ", tasks);
+  }
+
+  return (
+    <main>
+
+      <Header />
+
+      <section className={styles.content}>
+        <div className={styles.taskInfoContainer}>
+          <Input
+            onChange={() => {}}
+            value={''}
+          />
+          <Button onClick={handleNewAddTask}>
+            Criar
+            <PlusCircle size={16} color="#f2f2f2" weight="bold" />
+          </Button>
+        </div>
+
+        <div className={styles.tasksList}>
+          {tasks.length > 0 ? (
+            <div>
+              {tasks.map(function nomeDaFuncao(task) {
+                return (
+                  <Item
+                    key={task.id}
+                    data={task}
+                    removeTask={() => {}}
+                    toggleTaskStatus={() => {}}
+                  />
+                )
+              })}
+            </div>
+          ) : (
+            <Empty />
+          )}
+        </div>
+      </section>
+    </main>
+  )
+}
+>>>>>>> Stashed changes
