65cbfca2a08322329fdec52fab3b57e305873c7c
Emanuel <emanuelbrunobn@Rede.ulbra.br>
2025-03-12 21:16:48 -0300

    adicionado gestao de progressao e funcionalidade de atividade realizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { PlusCircle } from '@phosphor-icons/react'
+import { PlusCircle } from '@phosphor-icons/react';
 
-import styles from './App.module.css'
+import styles from './App.module.css';
 
-import { Button, Input, Empty, Item, Header } from './components'
+import { Button, Input, Empty, Item, Header } from './components';
 import { useState } from 'react';
 export interface ITask {
-  id: number
-  text: string
-  isChecked: boolean
+  id: number;
+  text: string;
+  isChecked: boolean;
 }
 
 // const BRANCH = 'aula04/gestor-tarefas';
@@ -16,36 +16,46 @@ const initialState: ITask[] = [];
 export function App() {
   const [tasks, setTasks] = useState(initialState);
   const [inputName, setInputName] = useState('');
+  const [totalTasks, setTotalTasks] = useState(0);
 
   function handleNewAddTask() {
-   if (inputName.trim().length <= 0) {
-    return;
-   }
+    if (inputName.trim().length <= 0) {
+      return;
+    }
 
-   const existTask = tasks.find(t => t.text === inputName);
+    const existTask = tasks.find((t) => t.text === inputName);
 
-   if (existTask) {
-    return;
-   }
-   
-   const newTask: ITask = {
-    id: Math.random(),
-    text: inputName,
-    isChecked: false,
-   };
-   setTasks((prevState) => [...prevState, newTask]);
+    if (existTask) {
+      return;
+    }
+
+    const newTask: ITask = {
+      id: Math.random(),
+      text: inputName,
+      isChecked: false,
+    };
+    setTasks((prevState) => [...prevState, newTask]);
+    setTotalTasks((prevTotal) => prevTotal + 1);
   }
 
+  const completedTasks = tasks.filter((task) => task.isChecked).length;
+
   function handleRemoveTask(id: number) {
-    const filterTasks = tasks.filter(task => task.id !== id);
+    const filterTasks = tasks.filter((task) => task.id !== id);
 
     setTasks(filterTasks);
     setInputName('');
   }
 
+  function toggleTaskStatus(id: number) {
+    setTasks((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === id ? { ...task, isChecked: !task.isChecked } : task
+      )
+    );
+  }
   return (
     <main>
-
       <Header />
 
       <section className={styles.content}>
@@ -58,7 +68,15 @@ export function App() {
             Criar
             <PlusCircle size={16} color="#f2f2f2" weight="bold" />
           </Button>
+          <div></div>
         </div>
+        <section style={{ marginBottom: '30px', textAlign: 'center' }}>
+          {' '}
+          <h1>
+            {' '}
+            Tarefas : {completedTasks} / {tasks.length} concluidas
+          </h1>
+        </section>
 
         <div className={styles.tasksList}>
           {tasks.length > 0 ? (
@@ -69,9 +87,9 @@ export function App() {
                     key={task.id}
                     data={task}
                     removeTask={handleRemoveTask}
-                    toggleTaskStatus={() => {}}
+                    toggleTaskStatus={toggleTaskStatus}
                   />
-                )
+                );
               })}
             </div>
           ) : (
@@ -80,5 +98,5 @@ export function App() {
         </div>
       </section>
     </main>
-  )
+  );
 }
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,28 +1,32 @@
-import { Trash, Check } from '@phosphor-icons/react'
+import { Trash, Check } from '@phosphor-icons/react';
 
-import { ITask } from '../../App'
+import { ITask } from '../../App';
 
-import styles from './Item.module.css'
+import styles from './Item.module.css';
 
 interface ItemProps {
-  data: ITask
-  removeTask: (id: number) => void
-  toggleTaskStatus: ({ id, value }: { id: number; value: boolean }) => void
+  data: ITask;
+  removeTask: (id: number) => void;
+  toggleTaskStatus: (id: number) => void;
 }
 
-export function Item({ data, removeTask }: ItemProps) {
-
+export function Item({ data, removeTask, toggleTaskStatus }: ItemProps) {
   const checkboxCheckedClassname = data.isChecked
     ? styles['checkbox-checked']
-    : styles['checkbox-unchecked']
+    : styles['checkbox-unchecked'];
   const paragraphCheckedClassname = data.isChecked
     ? styles['paragraph-checked']
-    : ''
+    : '';
 
   return (
     <div className={styles.container}>
       <div>
-        <label htmlFor="checkbox" onClick={() => {}}>
+        <label
+          htmlFor="checkbox"
+          onClick={() => {
+            toggleTaskStatus(data.id);
+          }}
+        >
           <input readOnly type="checkbox" checked={data.isChecked} />
           <span className={`${styles.checkbox} ${checkboxCheckedClassname}`}>
             {data.isChecked && <Check size={12} />}
@@ -38,5 +42,5 @@ export function Item({ data, removeTask }: ItemProps) {
         <Trash size={16} color="#808080" />
       </button>
     </div>
-  )
+  );
 }
